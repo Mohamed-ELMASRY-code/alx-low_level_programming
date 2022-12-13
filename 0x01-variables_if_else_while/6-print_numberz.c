9f3d7d07f3a5aa9b186d76b2142fd77a35550f7f
Mohamed-ELMASRY-code <mohamedelmasery12@gmail.com>
2022-12-12 16:23:48 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The entry point of C programs
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
